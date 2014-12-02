2548d26fef1eaa44c98c82bc446f454292bc5169
Arphunix <arphunix@gmail.com>
2014-12-02 22:08:57 +0100

    VAMOOOS

---
+++ HiSeSitor/Simulacion.java
@@ -0,0 +1,19 @@
+package HiSeSitor
+	public class Simulacion {
+		public Simulacion() {
+			int paus;
+			public Estado estado;
+			public correSimulacion() {
+			if (estado == null) {
+				estado = initGraph();
+			}
+		}
+
+		public addAleatOponents() {
+		}
+
+		public loadMap(){}
+		public escribeResultados() {}
+		public createGrafo() {}
+	}
+}
