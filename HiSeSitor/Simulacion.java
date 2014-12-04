a44b41fd31a3e4117ee4e63c4b539746274742a1
Arphunix <arphunix@gmail.com>
2014-12-04 15:57:08 +0100

    implementadas funciones de Simmulacion sin funcionalidad aun

--- HiSeSitor/Simulacion.java
+++ HiSeSitor/Simulacion.java
@@ -3,17 +3,27 @@ package HiSeSitor
 		public Simulacion() {
 			int paus;
 			public Estado estado;
+			public Sensores sensores;
+			public Node nodo;
 			public correSimulacion() {
-			if (estado == null) {
-				estado = initGraph();
+				if (estado == null) {
+					estado = initGraph();
+					addAleatOponents();
+				}
+				while((nodo = busca())!= null)
+				nodo = busca();
+				guardaValoresEstado();
+				updateEstado(nodo);
 			}
-		}
 
-		public addAleatOponents() {
-		}
+			public addAleatOponents() {
+			}
 
-		public loadMap(){}
-		public escribeResultados() {}
-		public createGrafo() {}
+			public loadMap(){}
+			public escribeResultados() {}
+			public createGrafo() {}
+			public correSimulacion() {
+
+		}
 	}
 }
