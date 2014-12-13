271ea9ef32d8201cde7c3cd7581dda236bd0b0df
Luis <Luis@Luis-PC>
2014-12-13 14:06:07 +0100

    en tu eclipse te vasiloh y en el mio marco estiloh!
    1 - loadMap cambia de Simulacion a Estado
    
    2 - variable definicionMalla en Estado: indica el porcentaje de
    definicion de la malla respecto al mapa. 100 indica que hay un nodo por
    cada unidad del mapa
    
    3 - metodo loadMap: lee un fichero y devuelve un array de enteros donde
    estarán las coordenadas de los obstaculos. el fichero tendrá al inicio
    X,Y como tamaño total del mapa seguido en cada linea del punto de inicio
    y del punto final del obstaculo. ej. "mapa1.txt"
    
    	10,10		//tamaño del mapa
    	8,1,8,5		//linea que empieza en el (8,1) y acaba en el (8,5)
    	6,6,6,9		//linea que empieza en el (6,6) y acaba en el (6,9)
    	1,7,4,7		//linea que empieza en el (1,7) y acaba en el (4,7)
    	
    4 - metodo initGraph: este metodo inicializa el grafo y crea los nodos
    dependiendo del tamaño del mapa y la definicion de la malla. para crear
    el grafo completo, usamos dos arrays auxiliares para guardar los nodos
    de la fila anterior y asi crear los vertices hacia arriba y en diagonal.
    una vez creado el grafo completo, se procede a quitar las aristas de los
    nodos que atraviesan algun obstaculo.(eso todavia falta)

--- src/HiSeSitor/Estado.java
+++ src/HiSeSitor/Estado.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Estado {
@@ -16,6 +17,7 @@ public class Estado {
 	public Nodo cazador;
 	public Nodo inicio;
 	public int presas = 0;
+	public int definicionMalla = 50;
 
 	public Estado() {
 		initHiddenNodes();
@@ -87,9 +89,84 @@ public class Estado {
 		return !isCalcula(x, s);
 	}
 
+	@SuppressWarnings("resource")
+	public int[] loadMap(){
+		File archivo = null;
+	    FileReader fr = null;
+	    BufferedReader br = null;
+	    int[] tamanoMapa = new int[1000];
+	    try {
+	         archivo = new File ("mapa1.txt");
+	         fr = new FileReader (archivo);
+	         br = new BufferedReader(fr);
+
+	         String linea;
+	         
+	         linea=br.readLine();
+	         String[] lineaTamano = linea.split(",");
+
+	         tamanoMapa[0] = Integer.parseInt(lineaTamano[0]);
+	         tamanoMapa[1] = Integer.parseInt(lineaTamano[1]);
+	         int i=2;
+	         while((linea=br.readLine())!=null){
+	        	 lineaTamano = linea.split(",");
+	        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[0]);
+	        	 i++;
+	        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[1]);
+	        	 i++;
+	        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[0]);
+	        	 i++;
+	        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[1]);
+	        	 i++;
+	         }
+	            
+	      }
+	      catch(Exception e){
+	         e.printStackTrace();
+	      }
+	    return tamanoMapa;
+	}
+	
 	// __OTROS NO GUILLE
 	public void initGraph() {
 		this.grafo = new Grafo();
+		
+		ArrayList<Nodo> lista1 = new ArrayList<Nodo>();
+		ArrayList<Nodo> lista2 = new ArrayList<Nodo>();
+		
+		int[] coordenadas = this.loadMap();
+		
+		int nNodosX = (coordenadas[0]*this.definicionMalla)/100;
+		int nNodosY = (coordenadas[1]*this.definicionMalla)/100;
+		
+		for(int i=0;i<nNodosY;i++){
+			
+			Nodo a = new Nodo(1, 0);
+			for(int j=0;j<nNodosX;j++){
+				
+				Nodo aux = new Nodo(1,0);
+				
+				if (lista1.get(j)!=null)
+					this.grafo.addEdge(1, a, lista1.get(j));
+				
+				if (lista1.get(j-1)!=null)
+					this.grafo.addEdge(1, a, lista1.get(j-1));
+				
+				if (lista1.get(j+1)!=null)
+					this.grafo.addEdge(1, a, lista1.get(j+1));
+				
+				lista2.add(aux);
+				this.grafo.addEdge(1, a, aux);
+				a = aux;
+			}
+			
+			lista1.clear();
+			lista1 = lista2;
+			lista2.clear();
+
+		}
+		
+		// falta quitar las aristas que atraviesan muros
 	}
 
 	public void estima() {
@@ -102,8 +179,8 @@ public class Estado {
 	public void guardaValoresEstado() {
 	}
 
-	public listaca getAdyacentes() {
-		return grafo.getAdyacentes();
+	public List<Nodo> getAdyacentes(Nodo n) {
+		return grafo.getAdjacents(n);
 	}
 
 	public void updateSensores() {
--- src/HiSeSitor/Simulacion.java
+++ src/HiSeSitor/Simulacion.java
@@ -10,7 +10,6 @@ import java.util.ArrayList;
 		public Estado estado;
 		public ArrayList<Sensor> sensores;
 		public Nodo nodo;		// repito, por que hay un nodo suelto por aqui???
-		public int definicionMalla = 50;
 		
 
 		public Simulacion() {
@@ -40,56 +39,12 @@ import java.util.ArrayList;
 				estado.addAleatOponent();
 		}
 
-		public int[] loadMap(){
-			File archivo = null;
-		    FileReader fr = null;
-		    BufferedReader br = null;
-		    int[] tamanoMapa;
-		    try {
-		         archivo = new File ("mapa1.txt");
-		         fr = new FileReader (archivo);
-		         br = new BufferedReader(fr);
-
-		         String linea;
-		         
-		         linea=br.readLine();
-		         String[] lineaTamano = linea.split(",");
-
-		         tamanoMapa[0] = Integer.parseInt(lineaTamano[0]);
-		         tamanoMapa[1] = Integer.parseInt(lineaTamano[1]);
-		         int i=2;
-		         while((linea=br.readLine())!=null){
-		        	 lineaTamano = linea.split(",");
-		        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[0]);
-		        	 i++;
-		        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[1]);
-		        	 i++;
-		        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[0]);
-		        	 i++;
-		        	 tamanoMapa[i]=Integer.parseInt(lineaTamano[1]);
-		        	 i++;
-		        	 
-		         }
-		            
-		      }
-		      catch(Exception e){
-		         e.printStackTrace();
-		      }
-		    return tamanoMapa;
-		}
 		
 		public void escribeResultados() {
 			
 		}
 		public void createGrafo() {
 			estado.initGraph();
-			int[] coordenadas = this.loadMap();
-			
-			int nNodos = (coordenadas[0]*coordenadas[1]*this.definicionMalla)/100;
-			
-			for(int i=0;i<nNodos;i++){
-				Nodo aux = new Nodo(1,0);
-				
 			}
 
 			
