85ee2ab2b51986c18af098cfce5357c3c67eee69
Luis <Luis@Luis-PC>
2014-12-29 01:04:16 +0100

    creada añadeDatos asi como otras funciones pequeñas necesarias. me falta un mini main para probar que funciona asi como la parte de procesamiento que lo dejo para mañana
    **IMPORTANTE: la cadena que me teneis que pasar tiene la siguiente
    forma:
    nombreEstrategia,nombreIteracion,victoria,nNodos,info
    
    para concatenar los valores, con poner:
    String s = estrategia +","+ iteracion +","+ victoria........;
    deberia valer

--- src/gestionDatos/Datos.java
+++ src/gestionDatos/Datos.java
@@ -4,17 +4,78 @@ import java.util.ArrayList;
 
 public class Datos {
 
-	private ArrayList<DatosEstrategia> datosEstrategias;
+	private ArrayList<DatosEstrategia> valoresEstrategias;
 
 	public Datos() {
-		this.datosEstrategias = new ArrayList<DatosEstrategia>();
+		this.valoresEstrategias = new ArrayList<DatosEstrategia>();
 	}
 
-	public ArrayList<DatosEstrategia> getDatosEstrategias() {
-		return datosEstrategias;
+	public ArrayList<DatosEstrategia> getValoresEstrategias() {
+		return valoresEstrategias;
+	}
+
+	public DatosEstrategia getValoresEstrategias(int i) {
+		return valoresEstrategias.get(i);
+	}
+
+	public int contieneEstrategia(String s) {
+
+		for (int i = 0; i < this.valoresEstrategias.size(); i++) {
+			if (this.getValoresEstrategias(i).getNombreEstrategia().equals(s))
+				return i;
+		}
+		return -1;
 	}
 
 	public void agregaDatos(String cadena) {
+
+		String[] recibido = cadena.split(",");
+		int auxEstr = this.contieneEstrategia(recibido[0]);
+		int auxIter = 0;
+
+		// si no existe ningna estrategia la creamos asi como la primera
+		// iteracion y la lectura que esta conlleva
+		if (auxEstr == -1) {
+			Lectura lectura = new Lectura(Integer.parseInt(recibido[2]),
+					Integer.parseInt(recibido[3]),
+					Integer.parseInt(recibido[4]));
+
+			DatosIteracion di = new DatosIteracion(recibido[1]);
+			DatosEstrategia de = new DatosEstrategia(recibido[0]);
+
+			di.getValoresLecturas().add(lectura);
+			de.getValoresIteraciones().add(di);
+			this.getValoresEstrategias().add(de);
+
+			// buscamos si existe la iteracion
+		} else {
+			auxIter = this.getValoresEstrategias(auxEstr).contieneIteracion(
+					recibido[1]);
+
+			// si no existe, la creamos asi como la lectura que conlleva
+			if (auxIter == -1) {
+				Lectura lectura = new Lectura(Integer.parseInt(recibido[2]),
+						Integer.parseInt(recibido[3]),
+						Integer.parseInt(recibido[4]));
+
+				DatosIteracion di = new DatosIteracion(recibido[1]);
+
+				di.getValoresLecturas().add(lectura);
+				this.getValoresEstrategias(auxEstr).getValoresIteraciones()
+						.add(di);
+
+				// creamos la lectura y la añadimos tal cual a la iteracion
+				// existente
+			} else {
+				Lectura lectura = new Lectura(Integer.parseInt(recibido[2]),
+						Integer.parseInt(recibido[3]),
+						Integer.parseInt(recibido[4]));
+
+				this.getValoresEstrategias(auxEstr)
+						.getValoresIteraciones(auxIter).getValoresLecturas()
+						.add(lectura);
+			}
+		}
 
 	}
 
--- src/gestionDatos/DatosEstrategia.java
+++ src/gestionDatos/DatosEstrategia.java
@@ -5,19 +5,32 @@ import java.util.ArrayList;
 public class DatosEstrategia {
 
 	private String nombreEstrategia;
-	private ArrayList<DatosIteracion> valoresEstrategia;
+	private ArrayList<DatosIteracion> valoresIteraciones;
 
 	public DatosEstrategia(String nombreEstrategia) {
 		this.nombreEstrategia = nombreEstrategia;
-		this.valoresEstrategia = new ArrayList<DatosIteracion>();
+		this.valoresIteraciones = new ArrayList<DatosIteracion>();
 	}
 
-	public String nombreEstrategia() {
+	public String getNombreEstrategia() {
 		return nombreEstrategia;
 	}
 
-	public ArrayList<DatosIteracion> getValores() {
-		return valoresEstrategia;
+	public ArrayList<DatosIteracion> getValoresIteraciones() {
+		return valoresIteraciones;
+	}
+
+	public DatosIteracion getValoresIteraciones(int i) {
+		return valoresIteraciones.get(i);
+	}
+
+	public int contieneIteracion(String s) {
+
+		for (int i = 0; i < this.valoresIteraciones.size(); i++) {
+			if (this.getValoresIteraciones(i).getNombreIteracion().equals(s))
+				return i;
+		}
+		return -1;
 	}
 
 }
